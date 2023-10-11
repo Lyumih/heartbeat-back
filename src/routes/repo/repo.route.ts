59e10ae504969abb527383819ab45c6596578a56
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-12 01:36:18 +0300

    modules

--- src/routes/repo/repo.route.ts
+++ src/routes/repo/repo.route.ts
@@ -1,8 +1,20 @@
 import { FastifyInstance } from 'fastify';
 
+type V = {
+  '@class': string,
+  '@rid': string,
+  '@version': number,
+}
+
+type Repo = {
+  title: string,
+  uri: string,
+}
+
 export const repoRouter = async (app: FastifyInstance) => {
 
+
   app.get("/", async function () {
-    return this.orient.query("select * from Repo").all()
+    return this.orient.query<Repo>("select * from Repo").all()
   })
 }
